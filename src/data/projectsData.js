54f250104004e2f26e1aabebee130d6993acd400
Tech_.erox <relic.of.sky@gmail.com>
2026-05-27 19:04:56 +0530

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,12 +1,25 @@
+
 const projectsData = [
   {
     id: 1,
-    title: 'HBP100 v2',
-    description: 'ML-powered entity validation and correction pipeline for noisy OCR and typo-heavy text. Improves accuracy by ~34%.',
-    tech: ['Python', 'NLP', 'Regex', 'sklearn'],
+    title: 'HBP100',
+    description:
+      'ML-based PII masking and validation pipeline designed for noisy, typo-heavy text. Combines entity extraction, regex validation, and correction workflows for cleaner structured outputs.',
+    tech: ['Python', 'numpy', 'Regex', 'sklearn', 'JSONL'],
     github: '#',
-    liveDemo: '#',
+    liveDemo: null,
   },
+
+  {
+    id: 2,
+    title: 'TuneX',
+    description:
+      'Automonous pid tuning software with llm and live changes.',
+    tech: ['platformio' , 'python' , 'pyside' , 'llm' ,],
+    github: '#',
+    liveDemo: null,
+  },
+
 ];
 
 export default projectsData;
