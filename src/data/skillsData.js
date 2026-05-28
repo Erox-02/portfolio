f2c91b8db4486e3191df4309c92acc7b12009e91
Tech_.erox <relic.of.sky@gmail.com>
2026-05-28 06:38:16 +0530

    Q

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,5 +1,5 @@
 const skillsData = [
-  'Python', 'Linux', 'Bash', 'Git', 'ML', 'System Optimization', 
+  'Python', 'Linux', 'Bash', 'Git', 'ML', 'System Optimization', 'llama.cpp ' , ' llm ' , ' lora training '
 ];
 
 export default skillsData;
