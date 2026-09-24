b503c0be7cb8787cfc29d9a40ad32dfe33369a15
Erox-02 <relic.of.sky@gmail.com>
2026-09-24 11:42:40 +0530

    changed css and app for new comps

---
+++ src/comps/label.jsx
@@ -0,0 +1,3 @@
+export default function Label({ children }) {
+  return <span className="mono">{children}</span>
+}
---
+++ src/comps/row.jsx
@@ -0,0 +1,3 @@
+export default function Row({ children }) {
+  return <li className="row">{children}</li>
+}
